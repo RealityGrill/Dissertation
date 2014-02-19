c22d7ea72ac3a7e341ec6eb9c55ef1201b651015
Matthew <matthew.johnson1@ncl.ac.uk>
2014-02-19 13:55:42 +0000

    third commit

--- project.clj
+++ project.clj
@@ -5,7 +5,11 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
   				[uk.org.russet/tawny-owl "1.0"]
-  				[pizza "1.0.0-SNAPSHOT"]]
+  				[pizza "1.0.0-SNAPSHOT"]
+           [org.semanticweb.elk/elk-owlapi "0.4.1"]
+                 [com.hermit-reasoner/org.semanticweb.hermit "1.3.8.3"]
+                 [net.sourceforge.owlapi/jfact "1.2.0"]
+                 [log4j/log4j "1.2.17"]]
   :main ^:skip-aot chooser.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/chooser/core.clj
+++ src/chooser/core.clj
@@ -4,24 +4,32 @@
         [tawny.reasoner]
         [pizza.pizza]
         [pizza.render-pizza])
+  (:require [tawny
+             [read]
+             [polyglot]
+             [reasoner :as r]
+             [pattern :as p]])
   (:gen-class))
 
 (defn chooserWindow []
 	(let [label (JLabel. "Stuff")
 		  labelb (JLabel. "otherStuff")	
+		  labelc (JLabel. "moreStuff")
 		  panel (JPanel.)
 		  frame (JFrame. "chooser")]
 		  (.setOpaque panel true)
 		  (.setText label (.toString PepperTopping))
 		  (.setText labelb (.toString InterestingPizza))
+		  (.setText labelc (tawny.reasoner/isubclasses pizza.pizza/pizzaontology PepperTopping))
 		  (.add panel label)
 		  (.add panel labelb)
+		  (.add panel labelc)
 		  (.setContentPane frame panel)
 		  (.setSize frame 300 100)
 		  (.setVisible frame true)))
 
 ;; so need to get reasoner working
 ;; fetching subclasses
-(tawny.reasoner/isubclasses pizza.pizza/pizzaontology PepperTopping)
+;(tawny.reasoner/isubclasses pizza.pizza/pizzaontology PepperTopping)
 
 ;; find annotation property called "label"
